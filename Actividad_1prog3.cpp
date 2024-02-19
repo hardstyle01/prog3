c917497e92d98053b47512b1ee8326e9ad13dc03
hardstyle01 <159567665+hardstyle01@users.noreply.github.com>
2024-02-19 13:18:24 -0400

    Actividad_1prog3.cpp

--- Actividad_1prog3.cpp
+++ Actividad_1prog3.cpp
@@ -5,94 +5,95 @@
 
 using namespace std;
 
-// Definición de las estructuras
+// DefiniciÃ³n de las estructuras
 struct LIBRO {
-	string title;
-	string author;
+	string titulo;
+	string autor;
 	string ISBN;
-	int publicationYear;
+	int aniopublicacion;
 };
 
 struct USUARIO {
 	int userID;
-	string name;
-	string lastname;
+	string nombre;
+	string apellido;
 	string email;
 };
 
 struct BIBLIOTECARIO {
-	int employeeID;
-	string name;
-	string lastname;
+	int trabajadorID;
+	string nombre;
+	string apellido;
 	string email;
 };
 
-// Función para generar una cadena aleatoria de un array dado
-string generateRandomString(const string arr[], int size) {
-	return arr[rand() % size]; // Selecciona una cadena aleatoria del array y la devuelve
+// FunciÃ³n para generar una cadena aleatoria de un array dado
+string cadenarandomica(const string arr[], int TAM) {
+	return arr[rand() % TAM]; // Selecciona una cadena aleatoria del array y la devuelve
 }
 
-// Función para generar un número entero aleatorio dentro de un rango dado
-int generateRandomInt(int min, int max) {
-	return rand() % (max - min + 1) + min; // Genera un número aleatorio dentro del rango y lo devuelve
+// FunciÃ³n para generar un nÃºmero entero aleatorio dentro de un rango dado
+int numerrorandomico(int min, int max) {
+	return rand() % (max - min + 1) + min; // Genera un nÃºmero aleatorio dentro del rango y lo devuelve
 }
 
-// Función para generar datos aleatorios para las estructuras
-void generateRandomData(LIBRO* libros, USUARIO* usuarios, BIBLIOTECARIO* bibliotecarios, int size) {
+// FunciÃ³n para generar datos aleatorios para las estructuras
+void DatosRandomicos(LIBRO* libros, USUARIO* usuarios, BIBLIOTECARIO* bibliotecarios, int TAM) {
 	// Arrays fijos de nombres y otros datos
 	string listaLibros[] = {"Aprende C", "Aprende PHP", "Aprende Linux", "Aprende Pyhton", "Aprende CISCO"};
-	string listaAutores[] = {"José Marquez", "Dillom Forner", "Agatha Linx", "Ruben Neres", "Ever Constantino"};
+	string listaAutores[] = {"JosÃ© Marquez", "Dillom Forner", "Agatha Linx", "Ruben Neres", "Ever Constantino"};
 	string listaISBN[] = {"1", "2", "3", "4", "5", "6", "7"};
 	string listaNombres[] = {"Jorge", "Ariel", "Norberto", "Alan", "Cinthia", "Yuliana"};
-	string listaApellidos[] = {"Gomez", "Ramirez", "Hernandez", "Andrade", "Foianini", "Peroné", "Santos"};
+	string listaApellidos[] = {"Gomez", "Ramirez", "Hernandez", "Andrade", "Foianini", "PeronÃ©", "Santos"};
 	
-	// Genera datos aleatorios para cada estructura en función del tamaño especificado
-	for (int i = 0; i < size; ++i) {
-		libros[i].title = generateRandomString(listaLibros, sizeof(listaLibros) / sizeof(listaLibros[0]));
-		libros[i].author = generateRandomString(listaAutores, sizeof(listaAutores) / sizeof(listaAutores[0]));
-		libros[i].ISBN = generateRandomString(listaISBN, sizeof(listaISBN) / sizeof(listaISBN[0]));
-		libros[i].publicationYear = generateRandomInt(2000, 2022);
+	// Genera datos aleatorios para cada estructura en funciÃ³n del tamaÃ±o especificado
+	for (int i = 0; i < TAM; ++i) {
+		libros[i].titulo = cadenarandomica(listaLibros, sizeof(listaLibros) / sizeof(listaLibros[0]));
+		libros[i].autor = cadenarandomica(listaAutores, sizeof(listaAutores) / sizeof(listaAutores[0]));
+		libros[i].ISBN = cadenarandomica(listaISBN, sizeof(listaISBN) / sizeof(listaISBN[0]));
+		libros[i].aniopublicacion = numerrorandomico(2000, 2022);
 		
 		usuarios[i].userID = i + 1;
-		usuarios[i].name = generateRandomString(listaNombres, sizeof(listaNombres) / sizeof(listaNombres[0]));
-		usuarios[i].lastname = generateRandomString(listaApellidos, sizeof(listaApellidos) / sizeof(listaApellidos[0]));
-		usuarios[i].email = usuarios[i].name + usuarios[i].lastname + "@gmail.com";
+		usuarios[i].nombre = cadenarandomica(listaNombres, sizeof(listaNombres) / sizeof(listaNombres[0]));
+		usuarios[i].apellido = cadenarandomica(listaApellidos, sizeof(listaApellidos) / sizeof(listaApellidos[0]));
+		usuarios[i].email = usuarios[i].nombre + usuarios[i].apellido + "@gmail.com";
 		
-		bibliotecarios[i].employeeID = 1000 + i;
-		bibliotecarios[i].name = generateRandomString(listaNombres, sizeof(listaNombres) / sizeof(listaNombres[0]));
-		bibliotecarios[i].lastname = generateRandomString(listaApellidos, sizeof(listaApellidos) / sizeof(listaApellidos[0]));
-		bibliotecarios[i].email = bibliotecarios[i].name + bibliotecarios[i].lastname + "@hotmail.com";
+		bibliotecarios[i].trabajadorID = 1000 + i;
+		bibliotecarios[i].nombre = cadenarandomica(listaNombres, sizeof(listaNombres) / sizeof(listaNombres[0]));
+		bibliotecarios[i].apellido = cadenarandomica(listaApellidos, sizeof(listaApellidos) / sizeof(listaApellidos[0]));
+		bibliotecarios[i].email = bibliotecarios[i].nombre + bibliotecarios[i].apellido + "@hotmail.com";
 	}
 }
 
 int main() {
-	srand(time(0)); // Inicializar la semilla para generar números aleatorios
+	srand(time(0)); // Inicializar la semilla para generar nÃºmeros aleatorios
 	
-	const int size = 3; // Tamaño de las estructuras
+	const int TAM = 3; // TamaÃ±o de las estructuras
 	
-	// Declaración de arrays de estructuras
-	LIBRO libros[size];
-	USUARIO usuarios[size];
-	BIBLIOTECARIO bibliotecarios[size];
+	// DeclaraciÃ³n de arrays de estructuras
+	LIBRO libros[TAM];
+	USUARIO usuarios[TAM];
+	BIBLIOTECARIO bibliotecarios[TAM];
 	
 	// Generar datos aleatorios para las estructuras
-	generateRandomData(libros, usuarios, bibliotecarios, size);
+	DatosRandomicos(libros, usuarios, bibliotecarios, TAM);
 	
 	// Imprimir los datos generados
 	cout << "LIBROS:" << endl;
-	for (int i = 0; i < size; ++i) {
-		cout << "Título: " << libros[i].title << ", Autor: " << libros[i].author << ", ISBN: " << libros[i].ISBN << ", Año de publicación: " << libros[i].publicationYear << endl;
+	for (int i = 0; i < TAM; ++i) {
+		cout << "TÃ­tulo: " << libros[i].titulo << ", Autor: " << libros[i].autor << ", ISBN: " << libros[i].ISBN << ", AÃ±o de publicaciÃ³n: " << libros[i].aniopublicacion << endl;
 	}
 	
 	cout << "\nUSUARIOS:" << endl;
-	for (int i = 0; i < size; ++i) {
-		cout << "ID: " << usuarios[i].userID << ", Nombre: " << usuarios[i].name << ", Apellido: " << usuarios[i].lastname << ", Email: " << usuarios[i].email << endl;
+	for (int i = 0; i < TAM; ++i) {
+		cout << "ID: " << usuarios[i].userID << ", Nombre: " << usuarios[i].nombre << ", Apellido: " << usuarios[i].apellido << ", Email: " << usuarios[i].email << endl;
 	}
 	
 	cout << "\nBIBLIOTECARIOS:" << endl;
-	for (int i = 0; i < size; ++i) {
-		cout << "ID de empleado: " << bibliotecarios[i].employeeID << ", Nombre: " << bibliotecarios[i].name << ", Apellido: " << bibliotecarios[i].lastname << ", Email: " << bibliotecarios[i].email << endl;
+	for (int i = 0; i < TAM; ++i) {
+		cout << "ID de empleado: " << bibliotecarios[i].trabajadorID << ", Nombre: " << bibliotecarios[i].nombre << ", Apellido: " << bibliotecarios[i].apellido << ", Email: " << bibliotecarios[i].email << endl;
 	}
 	
 	return 0;
 }
+
